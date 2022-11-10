e0567298b4e41799a0495076eebc2fddfec491b6
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-11-10 12:50:00 +0300

    Initial XCode project setup and creation

---
+++ yt-onedaybuild-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yt-onedaybuild-v2
+//
+//  Created by Kevin Nyangena on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
