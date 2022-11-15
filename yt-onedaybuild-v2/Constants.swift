d33dd5ff9d414650186249171bd6fca3943ddcc7
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-11-15 19:28:41 +0300

    Creating constants file and network test.

---
+++ yt-onedaybuild-v2/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  yt-onedaybuild-v2
+//
+//  Created by Kevin Nyangena on 11/15/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var API_KEY = "AIzaSyAtJXhqzPZvbfU_2TAMZWY0mcHhOTh5cj0"
+    static var PLAYLIST_ID = "PLMRqhzcHGw1aDYKmCuqXQ_IqpWpJlpoJ3"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
---
+++ yt-onedaybuild-v2/Video.swift
@@ -0,0 +1,19 @@
+//
+//  Video.swift
+//  yt-onedaybuild-v2
+//
+//  Created by Kevin Nyangena on 11/15/22.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoID = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+    
+}
