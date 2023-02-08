bd6c15cf8213d11a287e41fc93e39b3ea4d57ee7
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2023-02-08 19:47:32 +0300

    2023, Added and configured tableview, set UI elements to refresh on the main thread.

--- yt-onedaybuild-v2/Constants.swift
+++ yt-onedaybuild-v2/Constants.swift
@@ -12,4 +12,5 @@ struct Constants {
     static var API_KEY = "AIzaSyAtJXhqzPZvbfU_2TAMZWY0mcHhOTh5cj0"
     static var PLAYLIST_ID = "PLMRqhzcHGw1aDYKmCuqXQ_IqpWpJlpoJ3"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCELL_ID = "VideoCell"
 }
--- yt-onedaybuild-v2/ViewController.swift
+++ yt-onedaybuild-v2/ViewController.swift
@@ -7,18 +7,68 @@
 
 import UIKit
 
-var model = Model()
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ModelDelegate {
+    
+ 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var model = Model()
+    var videos = [Video]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        // Set the View Controller as the data source and the delegate
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Set the View Controller as the delegate of the model
+        model.delegate = self
 
         model.getVideos()
 
     }
-
-
+    
+    // MARK: - Model Delegate Methods
+    
+    func videosFetched(_ videos: [Video]) {
+        
+        // Set the returned videos to our video property
+        self.videos = videos
+        
+        // Refresh the tableview
+        tableView.reloadData()
+    }
+    
+    
+    // MARK: - TableView Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return videos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        
+        // Configure the cell with the data
+        
+        // Get the title for the video in question
+        let title = self.videos[indexPath.row].title
+        
+        cell.textLabel?.text = title
+        
+        
+        // Return the cell (give the cell back to the table view for display)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
 }
 
